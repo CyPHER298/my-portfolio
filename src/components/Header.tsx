3732b5b21a5a08d85bf95bd768dd417c6fba8f61
Henrique Batista <hbshenrique.ti@gmail.com>
2025-10-28 21:42:17 -0300

    Criando header e estilizando-o

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="title">Digital Portifolio Presentation</h1>
+      <div className="line-one"></div>
+      <div className="line-two"></div>
+    </header>
+  );
+}
